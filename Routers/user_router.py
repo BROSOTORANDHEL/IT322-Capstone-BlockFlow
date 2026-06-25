2b2614222ec1e6278b60e2f7e5341208b3271458
BROSOTORANDHEL <randhelbrosoto1@gmail.com>
2026-06-25 14:17:59 +0800

    Merge branch 'Brosoto' of https://github.com/BROSOTORANDHEL/IT322-Capstone-BlockFlow into Brosoto

--- Routers/user_router.py
+++ Routers/user_router.py
@@ -4,3 +4,4 @@ from Handlers.user_handler import login_handler
 router = APIRouter()
 
 router.post("/login")(login_handler)
+
--- database.py
+++ database.py
@@ -6,3 +6,4 @@ def get_db_connection():
     # This configuration makes SQLite return rows as dictionary-like objects
     conn.row_factory = sqlite3.Row
     return conn
+
